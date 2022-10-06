433bb64b2aeea604234c873f747ddf5e333a7286
Iandra <iandra.l.m.a@gmail.com>
2022-10-06 02:32:51 -0300

    Co-authored-by: rodarauj <rodarauj@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClientesModule } from './clientes/clientes.module';
+import { MomoComponent } from './momo/momo.component';
+import { HelloComponent } from './hello/hello.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MomoComponent,
+    HelloComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/momo/momo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'morzinho',
+  templateUrl: './momo.component.html',
+  styleUrls: ['./momo.component.css']
+})
+export class MomoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

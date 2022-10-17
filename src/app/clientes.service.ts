46ceb0cd11cb337a34b1fe199146d97911d9ff0b
Iandra <iandra.l.m.a@gmail.com>
2022-10-17 19:16:04 -0300

    oii

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -11,14 +11,26 @@ export class ClientesService {
   constructor(private httpClient: HttpClient) {
 
   }
-  dadosClienteMock(): Cliente {
+  dadosClienteMock(): Cliente[] {
     let cliente: Cliente = new Cliente();
+    let clientes: Cliente[] = [];
     cliente.nome = "Rodrigo";
     cliente.cpf = "565566566";
-    return cliente;
+    clientes.push(cliente);
+    cliente = new Cliente();
+    cliente.nome = "Iandra";
+    cliente.cpf = "5989878670";
+    cliente.idade = 24;
+    cliente.id = 2;
+    clientes.push(cliente);
+    return clientes;
   }
   salvar(cliente: Cliente): Observable<Cliente> {
     return this.httpClient.post<Cliente>("http://localhost:8080/api/clientes", cliente);
+
+  }
+  listarClientes(): Observable<Cliente[]> {
+    return this.httpClient.get<Cliente[]>("http://localhost:8080/api/clientes");
 
   }
 }

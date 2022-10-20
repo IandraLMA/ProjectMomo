cc02e5410ef5bd4991041e8b02d20f3fb8b4e6cd
Iandra <iandra.l.m.a@gmail.com>
2022-10-19 22:07:09 -0300

    novo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, Renderer2 } from '@angular/core';
 
 import jQuery from 'jquery'
 
@@ -10,23 +10,27 @@ import jQuery from 'jquery'
 export class AppComponent implements AfterViewInit {
   title = 'clientes-app';
 
-  ngAfterViewInit(){
-    (function($) {
-        "use strict";
-    
-        // Add active state to sidbar nav links
-        var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-                if (this.href === path) {
-                    $(this).addClass("active");
-                }
-            });
-    
-        // Toggle the side navigation
-        $("#sidebarToggle").on("click", function(e) {
-            e.preventDefault();
-            $("body").toggleClass("sb-sidenav-toggled");
-        });
+  constructor(private renderer: Renderer2) {
+
+  }
+
+  ngAfterViewInit() {
+    (function ($) {
+      "use strict";
+
+      // Add active state to sidbar nav links
+      var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+      $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
+        if (this.href === path) {
+          $(this).addClass("active");
+        }
+      });
+
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function (e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+      });
     })(jQuery);
   }
 }
